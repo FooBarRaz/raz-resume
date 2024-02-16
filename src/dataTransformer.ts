d15cd15a9ac8afb340b782e37335bbdb44c3c27c
Raz Ali <mehraz.hakim@gmail.com>
2024-02-15 21:37:46 -0500

    convert to typescript

--- src/dataTransformer.ts
+++ src/dataTransformer.ts
@@ -1,5 +1,5 @@
 import { ResumeSchema } from '@kurone-kito/jsonresume-types';
 import { raz } from './raz-resume';
 export const transform = (source: typeof raz): ResumeSchema => {
-    return source
-}
+    return source as any;
+} 
