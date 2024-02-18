cd3cd59dfe26c34a954be2a69140dbe84d31a6e3
Raz Ali <mehraz.hakim@gmail.com>
2024-02-18 15:36:29 -0500

    updates to resume content

--- src/dataTransformer.js
+++ src/dataTransformer.js
@@ -1,1 +1,42 @@
 "use strict";
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.transform = void 0;
+const mapOfSkillLevelsToDescriptions = {
+    90: "Master",
+    80: "Expert",
+    70: "Advanced",
+    60: "Proficient",
+    50: "Intermediate",
+    40: "Competent",
+    30: "Novice",
+    20: "Beginner",
+    10: "Fundamental Awareness",
+};
+const transform = (source) => {
+    return Object.assign(Object.assign({}, source), { work: source.work.map((work) => {
+            if (work.endDate === "Present") {
+                const { endDate } = work, restOfWork = __rest(work, ["endDate"]);
+                console.log(`returning rest of work for ${work.company}`);
+                return restOfWork;
+            }
+            return work;
+        }), skills: source.skills.map((skill) => {
+            return {
+                name: skill.name,
+                level: mapOfSkillLevelsToDescriptions[skill.level] || "Unknown",
+                keywords: skill.keywords,
+            };
+        }) });
+};
+exports.transform = transform;
--- src/index.js
+++ src/index.js
@@ -3,31 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_snapshot_1 = require("react-snapshot");
-const raz_resume_1 = require("./raz-resume");
-const react_awesome_resume_1 = __importDefault(require("react-awesome-resume"));
-require("semantic-ui-css/semantic.min.css");
 const react_1 = __importDefault(require("react"));
-const mapOfSkillLevelsToDescriptions = {
-    90: "Master",
-    80: "Expert",
-    70: "Advanced",
-    60: "Proficient",
-    50: "Intermediate",
-    40: "Competent",
-    30: "Novice",
-    20: "Beginner",
-    10: "Fundamental Awareness",
-};
-// Resume.defaultProps = {
-//   jsonResume: raz,
-//   theme: 'default',
-//   inline: false,
-// };
-// Resume.propTypes = {
-//   jsonResume: resumeType.isRequired,
-//   theme: PropTypes.string,
-//   inline: PropTypes.bool,
-// };
+const react_awesome_resume_1 = __importDefault(require("react-awesome-resume"));
+const react_snapshot_1 = require("react-snapshot");
+require("semantic-ui-css/semantic.min.css");
+const raz_resume_1 = require("./raz-resume");
 (0, react_snapshot_1.render)(react_1.default.createElement(react_awesome_resume_1.default, { jsonResume: raz_resume_1.raz, theme: "default" }), document.getElementById("root"));
 exports.default = react_awesome_resume_1.default;
