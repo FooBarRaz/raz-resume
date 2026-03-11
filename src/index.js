55c1f10a7d2c3db801154b603dfc1140105a5768
raz <mehraz.hakim@gmail.com>
2026-03-10 21:48:00 -0400

    Refactor index.js: remove react-awesome-resume and semantic UI imports, update render method to use App component

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
-const react_awesome_resume_1 = __importDefault(require("react-awesome-resume"));
 const react_snapshot_1 = require("react-snapshot");
-require("semantic-ui-css/semantic.min.css");
-const raz_resume_1 = require("./raz-resume");
-(0, react_snapshot_1.render)(react_1.default.createElement(react_awesome_resume_1.default, { jsonResume: raz_resume_1.raz, theme: "default" }), document.getElementById("root"));
-exports.default = react_awesome_resume_1.default;
+const App_1 = __importDefault(require("./App"));
+(0, react_snapshot_1.render)(react_1.default.createElement(App_1.default, null), document.getElementById('root'));
